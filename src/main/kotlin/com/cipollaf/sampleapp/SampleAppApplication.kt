ad9078457928182fd2f69bfdebf25383edb54836
Francesco Cipolla <cipolla@hikari-2.local>
2023-12-04 17:31:21 +0100

    Dummy Kotlin App + deployment workflow

---
+++ src/main/kotlin/com/cipollaf/sampleapp/SampleAppApplication.kt
@@ -0,0 +1,13 @@
+package com.cipollaf.sampleapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class SampleAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SampleAppApplication>(*args)
+}
+
---
+++ src/main/kotlin/com/cipollaf/sampleapp/controller/HelloController.kt
@@ -0,0 +1,13 @@
+package com.cipollaf.sampleapp.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    fun index(): String {
+        return "Hello World!"
+    }
+}
